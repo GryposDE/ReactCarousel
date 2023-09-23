37f9c3925e7626ee7c23fb1420302ef6638c1744
Christian Roth <chRoth@gmx.net>
2023-09-23 21:59:27 +0200

    Nice transition from slider edges

--- src/Carousel/Carousel.js
+++ src/Carousel/Carousel.js
@@ -10,51 +10,75 @@ function Carousel({ slides, parentWidth }) {
 
     const timerRef = useRef(null);
 
+    const [previousIndex, setPreviousIndex] = useState(0);
     const [currentIndex, setCurrentIndex] = useState(1);
 
-    // go to previous slide
+    // Go to previous slide
     const goToPrevious = () => {
       const isFirstSlide = currentIndex === 0;
       var newIndex = isFirstSlide ? slides.length : currentIndex - 1;
 
       let carousel = document.getElementById("CarouselSlideView");
-      if(isFirstSlide)
-      {      
-        carousel.style.transition = 'transform ease-out 0s';
-      }
-      else
-      {
-        carousel.style.transition = 'transform ease-out 0.8s';
-      }
+      carousel.style.transition = (isFirstSlide) ? 'transform ease-out 0s' : 'transform ease-out 0.8s';
 
+      setPreviousIndex(currentIndex);
       setCurrentIndex(newIndex);
     };
 
-    // go to next slide
+    // Go to next slide
     const goToNext = useCallback(() => {
-        const isLastSlide = currentIndex === slides.length +1;
+        const isLastSlide = currentIndex === slides.length + 1;
         var newIndex = isLastSlide ? 1 : currentIndex + 1;
 
-        let carousel = document.getElementById("CarouselSlideView");        
-        if(isLastSlide)
-        {
-            carousel.style.transition = 'transform ease-out 0s';
-        }
-        else
-        {
-            carousel.style.transition = 'transform ease-out 0.8s';
-        }
+        let carousel = document.getElementById("CarouselSlideView");  
+        carousel.style.transition = (isLastSlide) ? 'transform ease-out 0s' : 'transform ease-out 0.8s';
 
+        setPreviousIndex(currentIndex);
         setCurrentIndex(newIndex);
     }, [currentIndex, slides]);
 
-    // go to a slide
+    // Go to a slide
     const goToSlide = (slideIndex) => {
+      setPreviousIndex(currentIndex);
       setCurrentIndex(slideIndex);
     };
 
+    // Switches between edges of the carousel
+    const doOverflow = () => {
+      let carousel = document.getElementById("CarouselSlideView");  
+
+      if( (currentIndex === 0) && (previousIndex === 1) )
+      {
+        carousel.style.transition = 'transform ease-out 0s';
+        carousel.style.transform = `translateX(${-((slides.length) * parentWidth)}px)`
+        setCurrentIndex(slides.length);
+      }
+
+      if( (currentIndex === slides.length+1) && (previousIndex === slides.length))
+      {
+        carousel.style.transition = 'transform ease-out 0s';
+        carousel.style.transform = `translateX(${-((1) * parentWidth)}px)`
+        setCurrentIndex(1);
+      }
+    };
+
+    // Selects which dot is selected
+    const isSlideSelected = (slideIndex) => {
+      if(((currentIndex - 1) % slides.length) === slideIndex)
+      {
+        return 'gray';
+      }
+
+      if( (currentIndex === 0) && (slideIndex === slides.length -1) )
+      {
+        return 'gray';
+      }
+
+      return 'black';
+    }
+
   
-    // automatic switch slides every 2 sec
+    // Automatic switch slides every 2 sec
     useEffect(() => {
       if (timerRef.current) {
         clearTimeout(timerRef.current);
@@ -68,12 +92,13 @@ function Carousel({ slides, parentWidth }) {
 
 
     return (
-      <div className='Carousel'>
-        <h2> {currentIndex} </h2>
+      <div className='Carousel' style={{width:`${parentWidth}px`}}>
+        {/* <h5> previousIndex = {previousIndex} </h5> */}
+        {/* <h5> currentIndex = {currentIndex} </h5> */}
 
         {/* Arrow keys to manually move slide left/right */}
         <div>
-          <div className='leftArrow' onClick={goToPrevious} >
+          <div className='leftArrow' onClick={goToPrevious}>
             ❰
           </div>
 
@@ -82,22 +107,22 @@ function Carousel({ slides, parentWidth }) {
           </div>
         </div>
 
-        {/* show current slide */}
-        <div className='CarouselView'>
-            <div className='CarouselSlideView' id='CarouselSlideView' style={{transform: `translateX(${-((currentIndex) * parentWidth)}px)`, transition: 'transform ease-out 0.8s'}}>
+        {/* Show current slide */}
+        <div className='CarouselView' style={{width:`${parentWidth}px`}}>
+            <div className='CarouselSlideView' id='CarouselSlideView' style={{transform: `translateX(${-((currentIndex) * parentWidth)}px)`}} onTransitionEnd={doOverflow}>
 
                 <div className='CarouselSlide'> 
-                    <img src={slides[slides.length-1]} style={{objectFit: 'cover', width: '500px'}} alt={'image_first'} ></img>
+                    <img src={slides[slides.length-1].src} style={{objectFit: 'cover', width: `${parentWidth}px`}} alt={slides[slides.length-1].alt} ></img>
                 </div>
 
                 {slides.map((_, slideIndex) => (
                 <div className='CarouselSlide' key={slideIndex}> 
-                    <img src={slides[slideIndex]} style={{objectFit: 'cover', width: '500px'}} alt={'image'+slideIndex} ></img>
+                    <img src={slides[slideIndex].src} style={{objectFit: 'cover', width: `${parentWidth}px`}} alt={slides[slideIndex].alt} ></img>
                 </div>
                 ))}
 
                 <div className='CarouselSlide'> 
-                    <img src={slides[0]} style={{objectFit: 'cover', width: '500px',}} alt={'image_last'} ></img>
+                    <img src={slides[0].src} style={{objectFit: 'cover', width: `${parentWidth}px`}} alt={slides[0].alt} ></img>
                 </div>
 
           </div>
@@ -106,7 +131,7 @@ function Carousel({ slides, parentWidth }) {
         {/* Select slide via dot menu */}
         <div className='CarouselDotSelection'>
           {slides.map((slide, slideIndex) => (
-            <div className='CarouselDot' key={slideIndex} onClick={() => goToSlide(slideIndex+1)}>
+            <div className='CarouselDot' key={slideIndex} onClick={() => goToSlide(slideIndex+1)} style={{color:isSlideSelected(slideIndex)}}>
               ●
             </div>
           ))}
